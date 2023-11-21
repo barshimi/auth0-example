b66844a96cae815df9ddf892da68dd4bebcc4179
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-11-21 20:06:22 +0530

    feat: run app

--- apps/auth-example/scripts/seed.ts
+++ apps/auth-example/scripts/seed.ts
@@ -1,8 +1,6 @@
 import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import { customSeed } from "./customSeed";
-import { Salt, parseSalt } from "../src/auth/password.service";
-import { hash } from "bcrypt";
 
 if (require.main === module) {
   dotenv.config();
@@ -11,34 +9,12 @@ if (require.main === module) {
     console.error(error);
     process.exit(1);
   });
-  const salt = parseSalt(BCRYPT_SALT);
-
-  seed(salt).catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
 }
 
-async function seed(bcryptSalt: Salt) {
+async function seed() {
   console.info("Seeding database...");
 
   const client = new PrismaClient();
-
-  const data = {
-    email: "ashishpadhy1729@gmail.com",
-    name: "Ashish Padhy",
-    roles: ["admin"],
-    username: "Ashish Padhy",
-  };
-
-  await client.user.upsert({
-    where: {
-      username: data.username,
-    },
-
-    update: {},
-    create: data,
-  });
 
   const data = {
     email: "ashishpadhy1729@gmail.com",
